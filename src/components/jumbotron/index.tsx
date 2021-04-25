33e1350f378924df3190f25814b19c0f70b6f562
Francisco <javiermartincuenca@gmail.com>
2021-04-25 22:32:57 +0200

    Jumbotron added

---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,73 @@
+import React, { ImgHTMLAttributes } from 'react';
+import {
+  Item,
+  Inner,
+  Container,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbotron';
+
+interface JumbotronProps {
+  children: any;
+  direction: 'row' | 'row-reverse';
+}
+
+export default function Jumbotron({
+  children,
+  direction = 'row',
+  ...rest
+}: JumbotronProps) {
+  return (
+    <Item>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
+
+interface JumbotronSubComponentProps {
+  children: any;
+}
+
+interface JumbotronContainerProps extends JumbotronSubComponentProps {}
+
+interface JumbotronPaneProps extends JumbotronSubComponentProps {}
+
+interface JumbotronTitleProps extends JumbotronSubComponentProps {}
+
+interface JumbotronSubTitleProps extends JumbotronSubComponentProps {}
+
+interface JumbotronImageProps extends ImgHTMLAttributes<any> {}
+
+Jumbotron.Container = function JumbotronContainer({
+  children,
+  ...restProps
+}: JumbotronContainerProps) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({
+  children,
+  ...restProps
+}: JumbotronPaneProps) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({
+  children,
+  ...restProps
+}: JumbotronTitleProps) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({
+  children,
+  ...restProps
+}: JumbotronSubTitleProps) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage(props: JumbotronImageProps) {
+  return <Image {...props} />;
+};
---
+++ src/interfaces/jumboData.ts
@@ -0,0 +1,8 @@
+export interface JumboData {
+  id: number;
+  title: string;
+  subTitle: string;
+  image: string;
+  alt: string;
+  direction: 'row' | 'row-reverse';
+}
