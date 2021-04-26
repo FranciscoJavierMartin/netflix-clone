18a2f3807d18b62771897d4eada51346f5ae2bfd
Francisco <javiermartincuenca@gmail.com>
2021-04-26 12:39:06 +0200

    Add sub components for footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Column,
+  Link,
+  Title,
+  Text,
+  Break,
+} from './styles/footer';
+
+interface FooterProps {
+  children: React.ReactNode;
+}
+
+export default function Footer({ children, ...restProps }: FooterProps) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+interface FooterSubComponentProps {
+  children: React.ReactNode;
+}
+
+interface FooterRowProps extends FooterSubComponentProps {}
+interface FooterColumnProps extends FooterSubComponentProps {}
+interface FooterLinkProps extends FooterSubComponentProps {}
+interface FooterTitleProps extends FooterSubComponentProps {}
+interface FooterTextProps extends FooterSubComponentProps {}
+interface FooterBreakProps extends FooterSubComponentProps {}
+
+Footer.Row = function FooterRow({ children, ...restProps }: FooterRowProps) {
+  return <Row {...restProps}>{children}</Row>;
+};
+
+Footer.Column = function FooterColumn({
+  children,
+  ...restProps
+}: FooterColumnProps) {
+  return <Column {...restProps}>{children}</Column>;
+};
+
+Footer.Link = function FooterLink({ children, ...restProps }: FooterLinkProps) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Footer.Title = function FooterTitle({
+  children,
+  ...restProps
+}: FooterTitleProps) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Footer.Text = function FooterText({ children, ...restProps }: FooterTextProps) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Footer.Break = function FooterBreak({
+  children,
+  ...restProps
+}: FooterBreakProps) {
+  return <Break {...restProps}>{children}</Break>;
+};
