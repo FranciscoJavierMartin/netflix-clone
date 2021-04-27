bf79abb26653b5055940a0e0bef734e2ae22db30
Francisco <javiermartincuenca@gmail.com>
2021-04-27 09:26:55 +0200

    Raise font size for input email

--- src/components/opt-form/styles/opt-form.tsx
+++ src/components/opt-form/styles/opt-form.tsx
@@ -7,20 +7,21 @@ export const Container = styled.div`
   margin-top: 20px;
   flex-wrap: wrap;
 
-  @media (max-width: 1000px){
+  @media (max-width: 1000px) {
     flex-direction: column;
     align-items: center;
   }
-`
+`;
 
 export const Input = styled.input`
+  font-size: 20px;
   max-width: 450px;
   width: 100%;
   border: 0;
   padding: 10px;
   height: 70px;
   box-sizing: border-box;
-`
+`;
 
 export const Button = styled.button`
   display: flex;
@@ -38,7 +39,7 @@ export const Button = styled.button`
     background: #f40612;
   }
 
-  @media(max-width: 1000px){
+  @media (max-width: 1000px) {
     height: 50px;
     font-size: 16px;
     margin-top: 20px;
@@ -50,24 +51,24 @@ export const Button = styled.button`
     filter: brightness(0) invert(1);
     width: 24px;
 
-    @media(max-width: 1000px){
+    @media (max-width: 1000px) {
       width: 16px;
     }
   }
-`
+`;
 
 export const Text = styled.p`
   font-size: 19.2px;
   color: white;
   text-align: center;
 
-  @media(max-width: 600px){
+  @media (max-width: 600px) {
     font-size: 16px;
     line-height: 22px;
   }
-`
+`;
 
 export const Break = styled.div`
   flex-basis: 100%;
   height: 0;
-`
+`;
